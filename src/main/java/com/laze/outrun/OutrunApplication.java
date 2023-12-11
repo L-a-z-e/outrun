3f9bce4fa056aa4e0283ce91842e55ced90103a9
L-a-z-e <yysi8771@gmail.com>
2023-12-11 22:46:47 +0900

    First Commit

---
+++ src/main/java/com/laze/outrun/OutrunApplication.java
@@ -0,0 +1,13 @@
+package com.laze.outrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutrunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutrunApplication.class, args);
+    }
+
+}
